2f807900a1ab27d66f77437387acfe9612637e98
Bianca Cunha <mcunhabianca@gmail.com>
2024-11-25 17:48:02 -0300

    quarta aula 2

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,38 +1,53 @@
-import fs from 'fs';
-import {getTodosPosts, criarPost} from "../models/postsModel.js";
+import fs from 'fs'; // Importa o módulo fs para realizar operações no sistema de arquivos
+import {getTodosPosts, criarPost} from "../models/postsModel.js"; // Importa as funções para obter e criar posts do modelo de dados
 
+// Função assíncrona para listar todos os posts
 export async function listarPosts(req, res) {
-    // Obtém todos os posts através da função getTodosPosts
+  // Obtém todos os posts da base de dados usando a função getTodosPosts
     const posts = await getTodosPosts();
-    // Envia os posts como resposta JSON com status 200 (OK)
+  // Envia os posts como resposta JSON com status 200 (OK)
     res.status(200).json(posts);
 }
 
+// Função assíncrona para criar um novo post
 export async function criarNovoPost(req, res) {
+  // Obtém os dados do novo post do corpo da requisição
     const novoPost = req.body;
-    try  {
-        const postCriado = await criarPost(novoPost);
-        res.status(200).json(postCriado);
-    } catch(erro) {
-        console.error(erro.message);
-        res.status(500).json({'Erro': 'Falha na requisição.'});
+    try {
+    // Chama a função criarPost para inserir o novo post no banco de dados
+    const postCriado = await criarPost(novoPost);
+    // Envia o post criado como resposta JSON com status 200 (OK)
+    res.status(201).json(postCriado);
+    } catch (erro) {
+    // Imprime o erro no console para depuração
+    console.error(erro.message);
+    // Envia uma mensagem de erro como resposta JSON com status 500 (Internal Server Error)
+    res.status(500).json({'Erro': 'Falha na requisição.'});
     }
 }
 
+// Função assíncrona para fazer upload de uma imagem e criar um novo post
 export async function uploadImagem(req, res) {
+  // Cria um novo objeto de post com a descrição, URL da imagem e texto alternativo
     const novoPost = {
-        descricao: '',
-        imgUrl: req.file.originalname,
-        alt: ''
+    descricao: '',
+    imgUrl: req.file.originalname,
+    alt: ''
     };
 
-    try  {
-        const postCriado = await criarPost(novoPost);
-        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
-        fs.renameSync(req.file.path, imagemAtualizada);
-        res.status(200).json(postCriado);
-    } catch(erro) {
-        console.error(erro.message);
-        res.status(500).json({'Erro': 'Falha na requisição.'});
+    try {
+    // Chama a função criarPost para inserir o novo post no banco de dados
+    const postCriado = await criarPost(novoPost);
+    // Constrói o novo nome do arquivo com o ID do post
+    const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+    // Renomeia o arquivo para o novo nome
+    fs.renameSync(req.file.path, imagemAtualizada);
+    // Envia o post criado como resposta JSON com status 200 (OK)
+    res.status(201).json(postCriado);
+    } catch (erro) {
+    // Imprime o erro no console para depuração
+    console.error(erro.message);
+    // Envia uma mensagem de erro como resposta JSON com status 500 (Internal Server Error)
+    res.status(500).json({'Erro': 'Falha na requisição.'});
     }
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,28 +1,33 @@
-import express from 'express';
-import multer  from 'multer';
-import { criarNovoPost, listarPosts, uploadImagem } from '../controllers/postsController.js';
+import express from 'express'; // Importa o framework Express para criar a aplicação web
+import multer  from 'multer'; // Importa o módulo Multer para lidar com uploads de arquivos
+import { criarNovoPost, listarPosts, uploadImagem } from '../controllers/postsController.js'; // Importa as funções controladoras para manipular os posts
 
+// Configura o armazenamento de arquivos utilizando o multer.diskStorage
 const storage = multer.diskStorage({
+  // Define o diretório de destino para os arquivos
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+    cb(null, 'uploads/');
     },
+  // Define o nome do arquivo no destino
     filename: function (req, file, cb) {
-        cb(null, file.originalname);
+    cb(null, file.originalname);
     }
-})
+});
 
+// Cria uma instância do multer com a configuração de armazenamento
 const upload = multer({ dest: "./uploads" , storage});
 
 const routes = (app) => {
-    // Habilita o parser JSON para lidar com requisições JSON
+  // Habilita o parser JSON para lidar com requisições JSON
     app.use(express.json());
 
-    // Rota para obter todos os posts
+  // Rota para obter todos os posts (método GET)
     app.get('/posts', listarPosts);
 
-    // Rota para criar um post
+  // Rota para criar um novo post (método POST)
     app.post('/posts', criarNovoPost)
 
+  // Rota para fazer upload de imagem (método POST)
     app.post('/upload', upload.single('imagem'), uploadImagem);
 }
 
